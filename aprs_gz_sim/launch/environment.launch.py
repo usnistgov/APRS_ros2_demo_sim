e695a02cf37df3863280ee512863c12d80dcfb1d
Joseph Fernandez <jothomas0615@gmail.com>
2024-05-01 14:43:57 +0000

    Environment launching correctly

--- aprs_gz_sim/launch/environment.launch.py
+++ aprs_gz_sim/launch/environment.launch.py
@@ -18,24 +18,24 @@ from ament_index_python.packages import get_package_share_directory, PackageNotF
 
 
 def launch_setup(context, *args, **kwargs):
-    # urdf = os.path.join(get_package_share_directory('aprs_description'), 'urdf', 'aprs_lab_robots.urdf.xacro')
+    urdf = os.path.join(get_package_share_directory('aprs_description'), 'urdf', 'aprs_lab_robots.urdf.xacro')
     
-    # doc = xacro.process_file(urdf)
+    doc = xacro.process_file(urdf)
 
-    # robot_description_content = doc.toprettyxml(indent='  ')
+    robot_description_content = doc.toprettyxml(indent='  ')
 
-    # print(robot_description_content)
+    print(robot_description_content)
 
-    # robot_description = {"robot_description": robot_description_content}
+    robot_description = {"robot_description": robot_description_content}
 
-    # robot_state_publisher_node = Node(
-    #     package="robot_state_publisher",
-    #     executable="robot_state_publisher",
-    #     output="both",
-    #     parameters=[{"use_sim_time": True}, robot_description],
-    # )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[{"use_sim_time": True}, robot_description],
+    )
 
-    world_path = os.path.join(get_package_share_directory('aprs_description'), 'worlds', 'lab.sdf')
+    world_path = os.path.join(get_package_share_directory('aprs_gz_sim'), 'worlds', 'lab.sdf')
     
     gz = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -43,74 +43,74 @@ def launch_setup(context, *args, **kwargs):
                               'launch', 'gz_sim.launch.py')]),
             launch_arguments=[('gz_args', [' -r -v 4 '+ world_path])])
 
-    # gz_spawn_entity = Node(
-    #     package='ros_gz_sim',
-    #     executable='create',
-    #     output='screen',
-    #     arguments=['-string', doc.toxml(),
-    #                '-name', 'aprs_robots',
-    #                '-allow_renaming', 'true'],
-    # )
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-string', doc.toxml(),
+                   '-name', 'aprs_robots',
+                   '-allow_renaming', 'true'],
+    )
     
-    # joint_state_broadcaster = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     name="joint_state_broadcaster_spawner",
-    #     arguments=["joint_state_broadcaster"],
-    #     parameters=[
-    #         {"use_sim_time": True},
-    #     ],
-    # )
+    joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="joint_state_broadcaster_spawner",
+        arguments=["joint_state_broadcaster"],
+        parameters=[
+            {"use_sim_time": True},
+        ],
+    )
     
-    # ur_controller = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     name="ur_controller_spawner",
-    #     arguments=["ur_joint_trajectory_controller"],
-    #     parameters=[
-    #         {"use_sim_time": True},
-    #     ],
-    # )
+    ur_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="ur_controller_spawner",
+        arguments=["ur_joint_trajectory_controller"],
+        parameters=[
+            {"use_sim_time": True},
+        ],
+    )
     
-    # fanuc_controller = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     name="fanuc_controller_spawner",
-    #     arguments=["fanuc_joint_trajectory_controller"],
-    #     parameters=[
-    #         {"use_sim_time": True},
-    #     ],
-    # )
+    fanuc_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="fanuc_controller_spawner",
+        arguments=["fanuc_joint_trajectory_controller"],
+        parameters=[
+            {"use_sim_time": True},
+        ],
+    )
     
-    # franka_controller = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     name="franka_controller_spawner",
-    #     arguments=["franka_joint_trajectory_controller"],
-    #     parameters=[
-    #         {"use_sim_time": True},
-    #     ],
-    # )
+    franka_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="franka_controller_spawner",
+        arguments=["franka_joint_trajectory_controller"],
+        parameters=[
+            {"use_sim_time": True},
+        ],
+    )
     
-    # motoman_controller = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     name="motoman_controller_spawner",
-    #     arguments=["motoman_joint_trajectory_controller"],
-    #     parameters=[
-    #         {"use_sim_time": True},
-    #     ],
-    # )
+    motoman_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        name="motoman_controller_spawner",
+        arguments=["motoman_joint_trajectory_controller"],
+        parameters=[
+            {"use_sim_time": True},
+        ],
+    )
 
     nodes_to_start = [
         gz,
-        # robot_state_publisher_node,
-        # gz_spawn_entity,
-        # joint_state_broadcaster,
-        # ur_controller,
-        # fanuc_controller,
-        # franka_controller,
-        # motoman_controller
+        robot_state_publisher_node,
+        gz_spawn_entity,
+        joint_state_broadcaster,
+        ur_controller,
+        fanuc_controller,
+        franka_controller,
+        motoman_controller
     ]
 
     return nodes_to_start
