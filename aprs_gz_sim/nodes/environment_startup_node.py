54bd064129c1212077206b136bf50ee85de41bc9
Joseph Fernandez <joseph.fernandez@nist.gov>
2024-06-27 16:50:45 +0000

    Gripper connected to robots and oriented correctly

--- aprs_gz_sim/nodes/environment_startup_node.py
+++ aprs_gz_sim/nodes/environment_startup_node.py
@@ -2,7 +2,7 @@
 
 from aprs_gz_sim.evironment_startup import EnvironmentStartup
 from time import sleep
-from random import randint
+from random import randint, random
 
 import rclpy
 
@@ -13,12 +13,12 @@ def main():
 
     sleep(10)
     
-    startup_node.spawn_sensors("advanced_logical_camera", "advanced_logical_camera", [-1.0, 0.0, 1.5])
+    startup_node.spawn_sensors("advanced_logical_camera", "advanced_logical_camera", [0.0, 0.0, 5.0])
     # startup_node.spawn_sensors("advanced_logical_camera", "advanced_logical_camera", [-0.85, 0.5, 1.5])
 
     part_type = "battery"
     part_color = "blue"
-    startup_node.spawn_part(part_type, part_color, [-1.0, 0.0, 1.5])
+    startup_node.spawn_part(part_type, part_color, [0.1, -0.073025, 0.9])
     
     # part_type = ["battery", "pump", "regulator", "sensor"][randint(0,3)]
     # part_color = ["blue", "green", "red", "purple", "orange"][randint(0,4)]
