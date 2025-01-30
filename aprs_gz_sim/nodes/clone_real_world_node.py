2d237205148950ec376495b410891bb4eb8df4ac
Joseph Fernandez <joseph.fernandez@nist.gov>
2025-01-30 17:30:02 +0000

    Added code to spawn teach table trays

--- aprs_gz_sim/nodes/clone_real_world_node.py
+++ aprs_gz_sim/nodes/clone_real_world_node.py
@@ -21,6 +21,9 @@ class CloneNode(Node):
         self.spawner_node = EnvironmentStartup()
         self.motoman_trays_spawned = False
         self.fanuc_trays_spawned = False
+        self.teach_trays_spawned = False
+        
+        self.trays_spawned = [self.fanuc_trays_spawned, self.motoman_trays_spawned, self.teach_trays_spawned]
         
         vision_quaternion = quaternion_from_euler(0, 0, -pi/2)
         vision_orientation = Quaternion()
@@ -31,9 +34,11 @@ class CloneNode(Node):
 
         self.fanuc_vision_pose_ = build_pose(-1.0, 0.75, 0.9, vision_orientation)
         self.motoman_vision_pose_ = build_pose(0.0, 0.75, 0.9, vision_orientation)
+        self.teach_vision_pose_ = build_pose(-1.95505, -2.181225, 0.77, vision_orientation)
         
         fanuc_trays_info_sub = self.create_subscription(Trays, '/fanuc/table_trays_info', self.update_fanuc_trays, 10)
         motoman_trays_info_sub = self.create_subscription(Trays, '/motoman/table_trays_info', self.update_motoman_trays, 10)
+        teach_trays_info_sub = self.create_subscription(Trays, '/teach/table_trays_info', self.update_teach_trays, 10)
         
         
     
@@ -61,7 +66,7 @@ class CloneNode(Node):
             self.spawner_node.spawn_tray(tray_type, tray_color, xyz, rotation, occupied_slots)
         self.motoman_trays_spawned = True
 
-        if self.fanuc_trays_spawned:
+        if all(self.trays_spawned):
             self.spawner_node.environment_ready()
     
     def update_fanuc_trays(self, msg: Trays):
@@ -88,7 +93,34 @@ class CloneNode(Node):
             self.spawner_node.spawn_tray(tray_type, tray_color, xyz, rotation, occupied_slots)
         self.fanuc_trays_spawned = True
 
-        if self.motoman_trays_spawned:
+        if all(self.trays_spawned):
+            self.spawner_node.environment_ready()
+            
+    def update_teach_trays(self, msg: Trays):
+        if self.teach_trays_spawned:
+            return
+        all_trays: list[Tray] = msg.kit_trays + msg.part_trays
+        
+        for tray in all_trays:
+            world_pose = multiply_pose(self.teach_vision_pose_, tray.tray_pose.pose)
+            
+            tray_type = self.tray_types_[tray.identifier - 13]
+            tray_color = "black"
+            
+            xyz = [world_pose.position.x, world_pose.position.y, world_pose.position.z]
+            
+            rotation = rad_to_deg(rpy_from_quaternion(world_pose.orientation)[-1])
+            
+            occupied_slots = []
+            for slot in tray.slots:
+                slot: SlotInfo
+                if slot.occupied:
+                    occupied_slots.append("_".join(slot.name.split("_")[-2:]))
+            
+            self.spawner_node.spawn_tray(tray_type, tray_color, xyz, rotation, occupied_slots)
+        self.teach_trays_spawned = True
+
+        if all(self.trays_spawned):
             self.spawner_node.environment_ready()
 
 if __name__ == "__main__":
